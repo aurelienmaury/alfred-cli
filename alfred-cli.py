526a960c9b47758151cb1ffa0a95e0ff01080b8d
A. Maury <aurelien.maury+localgit@wescale.fr>
2015-12-14 16:16:23 +0100

    trying to introduce feedback from Alfred

--- alfred-cli.py
+++ alfred-cli.py
@@ -6,23 +6,40 @@ import os
 from cmd2 import Cmd
 
 global spine_input
+global spine_output
+
+zmq_ctx = zmq.Context()
 
 
 class AlfredCli(Cmd):
-    def do_connect(self, line):
-        connect(line)
+    def do_publish_to(self, line):
+        connect_publish(line)
+
+    def do_subscribe_to(self, line):
+        connect_subscribe(line)
 
     def do_msg(self, line):
         global spine_input
         spine_input.send(line)
 
 
-def connect(target_socket):
-    zmq_ctx = zmq.Context()
+def connect_publish(target_socket):
     global spine_input
+
     spine_input = zmq_ctx.socket(zmq.PUSH)
     spine_input.connect(target_socket)
+
     print "Connected to: " + target_socket
+
+
+def connect_subscribe(target_socket):
+    global spine_output
+
+    spine_output = zmq_ctx.socket(zmq.SUB)
+    spine_output.setsockopt(zmq.SUBSCRIBE, '/alfred/cli-output/')
+    spine_output.connect(target_socket)
+
+    print "Subscribed to: " + target_socket
 
 
 def main():
